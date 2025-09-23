b0b0c6ff73238a0cd9d9cb17288ac2c207ec6452
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-09-23 09:20:06 -0700

    3.7: Add new (failing) test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -26,3 +26,23 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("{app_address}/subscriptions"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
