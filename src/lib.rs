d221deae29175444466563e87999eb36549ca348
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-09-24 09:00:13 -0700

    Split into multiple files

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,3 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, web};
-use std::net::TcpListener;
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
