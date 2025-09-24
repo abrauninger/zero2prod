8c34949c3cd257597f8284df3b962ea366738bdb
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-09-24 13:23:50 -0700

    3.9: Attempt to use 'sqlx::query!'; doesn't build yet

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,25 @@
 use actix_web::{HttpResponse, web};
+use chrono::Utc;
+use sqlx::PgConnection;
+use uuid::Uuid;
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(
+    _form: web::Form<FormData>,
+    connection: web::Data<PgConnection>,
+) -> HttpResponse {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now(),
+    )
+    .execute(connection.get_ref())
+    .await;
+
     HttpResponse::Ok().finish()
 }
 
