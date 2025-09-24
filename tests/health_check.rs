cc1ee228c58cf6cf8095169394afcce27b0dd5ee
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-09-24 09:23:51 -0700

    3.8: Add test code to query database

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -37,7 +37,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-    let _connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres");
 
@@ -55,6 +55,11 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
 }
 
 #[tokio::test]
