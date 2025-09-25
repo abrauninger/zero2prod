bcca5aef1f87a5fcae12069579ab37b06e4816a2
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-09-25 11:37:37 -0700

    4.5: Migrate from 'log' to 'tracing'

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -6,14 +6,14 @@ use uuid::Uuid;
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
 
-    log::info!(
+    tracing::info!(
         "request_id {} - Adding '{}' '{}' as a new subscriber.",
         request_id,
         form.email,
         form.name
     );
 
-    log::info!(
+    tracing::info!(
         "request_id {} - Saving new subscriber details in the database",
         request_id,
     );
@@ -32,11 +32,11 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .await
     {
         Ok(_) => {
-            log::info!("request_id {request_id} - New subscriber details have been saved");
+            tracing::info!("request_id {request_id} - New subscriber details have been saved");
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("request_id {request_id} - Failed to execute query: {e:?}");
+            tracing::error!("request_id {request_id} - Failed to execute query: {e:?}");
             HttpResponse::InternalServerError().finish()
         }
     }
