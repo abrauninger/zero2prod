c20165f2d4889c1237b250a65ae8862725250152
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-09-25 14:00:10 -0700

    4.5: Extract 'get_tracing_subscriber'

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
+use tracing::{Subscriber, subscriber::set_global_default};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
@@ -12,13 +12,7 @@ async fn main() -> Result<(), std::io::Error> {
     // Redirect all `log` events to our subscriber
     LogTracer::init().expect("Failed to set logger");
 
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
+    let subscriber = get_tracing_subscriber("zero2prod".into(), "into".into());
 
     set_global_default(subscriber).expect("Failed to set subscriber");
 
@@ -31,4 +25,15 @@ async fn main() -> Result<(), std::io::Error> {
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to port.");
     run(listener, connection_pool)?.await
+}
+
+fn get_tracing_subscriber(name: String, env_filter: String) -> impl Subscriber {
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
 }
