79013caecacfa0e026bc7d004f8569607383764f
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-09-29 21:53:28 -0700

    7.3: Extract 'startup::build' out of main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,12 @@
-use sqlx::PgPool;
-use std::net::TcpListener;
-use zero2prod::{
-    configuration::get_configuration, email_client::EmailClient, startup::run, telemetry,
-};
+use zero2prod::{configuration::get_configuration, startup::build, telemetry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     telemetry::init_subscriber(subscriber);
 
-    // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-
-    let connection_pool = PgPool::connect_with(configuration.database.connect_options())
-        .await
-        .expect("Failed to connect to Postgres");
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-
-    let timeout = configuration.email_client.timeout();
-
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address).expect("Failed to bind to port.");
-    run(listener, connection_pool, email_client)?.await
+    let server = build(configuration).await?;
+    server.await?;
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 use actix_web::{
@@ -8,6 +9,33 @@ use actix_web::{
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool = PgPool::connect_with(configuration.database.connect_options())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+
+    let timeout = configuration.email_client.timeout();
+
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address).expect("Failed to bind to port.");
+    run(listener, connection_pool, email_client)
+}
 
 pub fn run(
     listener: TcpListener,
