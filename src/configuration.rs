64ef71a39c182c13d67e1b51f2d0b9d94209a947
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-09-30 12:45:50 -0700

    7.7: Implement the rest of the subscription-confirmation flow

--- src/configuration.rs
+++ src/configuration.rs
@@ -17,8 +17,6 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
 
     let environment_filename = format!("{}.yaml", environment.as_str());
 
-    dbg!(&environment_filename);
-
     let settings = config::Config::builder()
         .add_source(config::File::from(
             configuration_directory.join("base.yaml"),
