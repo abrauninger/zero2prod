2b4394c3f96a0ebe0590708ef00e36b2a54cd7c2
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-09-30 14:08:53 -0700

    8.2: Add 'error_chain_fmt'

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -100,7 +100,7 @@ impl ResponseError for StoreTokenError {}
 
 impl std::fmt::Debug for StoreTokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}\nCaused by:\n\t{}", self, self.0)
+        error_chain_fmt(self, f)
     }
 }
 
@@ -117,6 +117,19 @@ impl std::error::Error for StoreTokenError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         Some(&self.0)
     }
+}
+
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{e}\n")?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{cause}")?;
+        current = cause.source();
+    }
+    Ok(())
 }
 
 #[tracing::instrument(
