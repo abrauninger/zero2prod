751007fd38a5a9202908f460f96e4a2b50570cd1
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-01 10:57:00 -0700

    10.2: Start using Argon2

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -7,9 +7,9 @@ use actix_web::{
     web,
 };
 use anyhow::Context;
+use argon2::{Algorithm, Argon2, Params, PasswordHasher, Version};
 use base64::Engine;
 use secrecy::{ExposeSecret, Secret};
-use sha3::Digest;
 use sqlx::PgPool;
 
 use crate::{domain::SubscriberEmail, email_client::EmailClient, routes::error_chain_fmt};
@@ -107,29 +107,51 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
+    let hasher = Argon2::new(
+        Algorithm::Argon2id,
+        Version::V0x13,
+        // "Use Argon2id with a minimum configuration of 19 MiB of memory, an iteration count of 2, and 1 degree of parallelism."
+        // https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html (2025-10-01)
+        // Use the default output_len, which is 32 bytes.
+        Params::new(
+            /*m_cost:*/ 19000, /*t_cost:*/ 2, /*p_cost: */ 1,
+            /*output_len:*/ None,
+        )
+        .context("Failed to build Argon2 parameters")
+        .map_err(PublishError::UnexpectedError)?,
+    );
 
-    // Lowercase hexadecimal encoding
-    let password_hash = format!("{:x}", password_hash);
-
-    let user_id: Option<_> = sqlx::query!(
+    let row: Option<_> = sqlx::query!(
         r#"
-        SELECT user_id
+        SELECT user_id, password_hash, salt
         FROM users
-        WHERE username = $1 AND password_hash = $2
+        WHERE username = $1
         "#,
-        credentials.username,
-        password_hash,
+        credentials.username
     )
     .fetch_optional(pool)
     .await
-    .context("Failed to perform a query to validate auth credentials")
+    .context("Failed to perform a query to retrieve stored credentials.")
     .map_err(PublishError::UnexpectedError)?;
 
-    user_id
-        .map(|row| row.user_id)
-        .ok_or_else(|| anyhow::anyhow!("Invalid username or password"))
-        .map_err(PublishError::AuthError)
+    let (expected_password_hash, user_id, salt) = match row {
+        Some(row) => (row.password_hash, row.user_id, row.salt),
+        None => return Err(PublishError::AuthError(anyhow::anyhow!("Unknown username"))),
+    };
+
+    let password_hash = hasher
+        .hash_password(credentials.password.expose_secret().as_bytes(), &salt)
+        .context("Failed to hash password")
+        .map_err(PublishError::UnexpectedError)?;
+
+    // Lowercase hexadecimal encoding
+    let password_hash = format!("{:x}", password_hash.hash.unwrap());
+
+    if password_hash != expected_password_hash {
+        Err(PublishError::AuthError(anyhow::anyhow!("Invalid password")))
+    } else {
+        Ok(user_id)
+    }
 }
 
 struct ConfirmedSubscriber {
