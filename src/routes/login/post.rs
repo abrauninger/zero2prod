a4d4d7a7d7079cc8d2a31c60ef91674481795110
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-01 13:03:10 -0700

    10.6: Redirect on POST /login

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,7 @@
-use actix_web::HttpResponse;
+use actix_web::{HttpResponse, http::header::LOCATION};
 
 pub async fn login() -> HttpResponse {
-    HttpResponse::Ok().finish()
+    HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/"))
+        .finish()
 }
