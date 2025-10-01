53ea424cff1531533cafbe2c9d86152229bb64a3
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-01 11:09:11 -0700

    10.2: Finish using Argon2

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,6 +1,7 @@
+use argon2::password_hash::SaltString;
+use argon2::{Argon2, PasswordHasher};
 use reqwest::Url;
 use secrecy::Secret;
-use sha3::Digest;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::sync::LazyLock;
 use uuid::Uuid;
@@ -142,8 +143,11 @@ impl TestUser {
     }
 
     async fn store(&self, pool: &PgPool) {
-        let password_hash = sha3::Sha3_256::digest(self.password.as_bytes());
-        let password_hash = format!("{:x}", password_hash);
+        let salt = SaltString::generate(&mut rand::thread_rng());
+        let password_hash = Argon2::default()
+            .hash_password(self.password.as_bytes(), &salt)
+            .unwrap()
+            .to_string();
 
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash)
