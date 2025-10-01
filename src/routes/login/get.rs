1243b8e75a34fbc336906d1c199b598091f739f0
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-01 14:29:04 -0700

    10.6: Gracefully handle malformed query parameters

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,4 +1,8 @@
 use actix_web::{HttpResponse, http::header::ContentType, web};
+use hmac::{Hmac, Mac};
+use secrecy::ExposeSecret;
+
+use crate::startup::HmacSecret;
 
 #[derive(serde::Deserialize)]
 pub struct QueryParams {
@@ -6,13 +10,39 @@ pub struct QueryParams {
     tag: String,
 }
 
-pub async fn login_form(query: Option<web::Query<QueryParams>>) -> HttpResponse {
+impl QueryParams {
+    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
+        let tag = hex::decode(self.tag)?;
+        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
+
+        let mut mac =
+            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
+        mac.update(query_string.as_bytes());
+        mac.verify_slice(&tag)?;
+
+        Ok(self.error)
+    }
+}
+
+pub async fn login_form(
+    query: Option<web::Query<QueryParams>>,
+    secret: web::Data<HmacSecret>,
+) -> HttpResponse {
     let error_html = match query {
         None => "".into(),
-        Some(query) => format!(
-            "<p><i>{}</i></p>",
-            htmlescape::encode_minimal(&query.0.error)
-        ),
+        Some(query) => match query.0.verify(&secret) {
+            Ok(error) => {
+                format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error))
+            }
+            Err(e) => {
+                tracing::warn!(
+                    error.message = %e,
+                    error.cause_chain = ?e,
+                    "Failed to viery query parameters using the HMAC tag"
+                );
+                "".into()
+            }
+        },
     };
 
     HttpResponse::Ok()
