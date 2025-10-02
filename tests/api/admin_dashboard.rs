7671a18b5f4cda79a766d3eb2d8ca4d5ae639353
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-02 12:59:46 -0700

    10.8: Add failing test: 'new_password_fields_must_match'

--- tests/api/admin_dashboard.rs
+++ tests/api/admin_dashboard.rs
@@ -1,3 +1,5 @@
+use uuid::Uuid;
+
 use crate::helpers::{assert_is_redirect_to, spawn_app};
 
 #[tokio::test]
@@ -10,4 +12,34 @@ async fn you_must_be_logged_in_to_access_the_admin_dashboard() {
 
     // Assert
     assert_is_redirect_to(&response, "/login");
+}
+
+#[tokio::test]
+async fn new_password_fields_must_match() {
+    // Arrange
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let another_new_password = Uuid::new_v4().to_string();
+    assert_ne!(new_password, another_new_password);
+
+    // Act - Part 1 - Login
+    app.post_login(&serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password,
+    }))
+    .await;
+
+    // Act - Part 2 - Try to change password
+    let response = app
+        .post_change_password(&serde_json::json!({
+            "current_password": &app.test_user.password,
+            "new_password": &new_password,
+            "new_password_check": &another_new_password,
+        }))
+        .await;
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // Act - Part 3 - Follow the redirect
+    let html_page = app.get_change_password_html().await;
+    assert!(!html_page.contains("You entered two different new passwords"));
 }
