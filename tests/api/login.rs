cd6023c5031cab3ce3a094fbd75c801f1120ddb6
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-01 23:02:09 -0700

    10.6: Use 'cookies' feature in 'reqwest'

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -19,11 +19,6 @@ async fn error_flash_message_is_set_on_failure() {
     // Assert
     assert_is_redirect_to(&response, "/login");
 
-    let cookies: HashSet<_> = response
-        .headers()
-        .get_all("Set-Cookie")
-        .into_iter()
-        .collect();
-
-    assert!(cookies.contains(&HeaderValue::from_str("_flash=Authentication failed").unwrap()));
+    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
+    assert_eq!(flash_cookie.value(), "Authentication failed");
 }
