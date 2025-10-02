ee3a84f926b3e97794f13bc988bdf2af6420f456
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-02 12:16:35 -0700

    10.8: Migration: Add seed user

---
+++ migrations/20251002190936_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    '850180d7-4fa0-4963-9218-e2ea3942e1aa',
+    'admin',
+    '$argon2d$v=19$m=15000,t=2,p=1$at5SGu7yxvK/Bw283Hh2PA$vH/V6oiuXxDsOmtSy013veVWE8gN8lji3/1iHfHZSZg'
+);
