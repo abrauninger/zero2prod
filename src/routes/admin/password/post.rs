1e65b9d513802fc3513748204732549c7bb5da30
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-02 13:21:35 -0700

    10.8: Make test pass

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,8 +1,11 @@
 use actix_web::{HttpResponse, web};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
+use sqlx::PgPool;
 
 use crate::{
+    authentication::{AuthError, Credentials, validate_credentials},
+    routes::admin::dashboard::get_username,
     session_state::TypedSession,
     utils::{e500, see_other},
 };
@@ -17,10 +20,13 @@ pub struct FormData {
 pub async fn change_password(
     form: web::Form<FormData>,
     session: TypedSession,
+    pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    if session.get_user_id().map_err(e500)?.is_none() {
+    let user_id = session.get_user_id().map_err(e500)?;
+
+    let Some(user_id) = user_id else {
         return Ok(see_other("/login"));
-    }
+    };
 
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
@@ -28,6 +34,23 @@ pub async fn change_password(
         )
         .send();
         return Ok(see_other("/admin/password"));
+    }
+
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
+
+    let credentials = Credentials {
+        username,
+        password: form.0.current_password,
+    };
+
+    if let Err(e) = validate_credentials(credentials, &pool).await {
+        return match e {
+            AuthError::InvalidCredentials(_) => {
+                FlashMessage::error("The current password is incorrect.").send();
+                Ok(see_other("/admin/password"))
+            }
+            AuthError::UnexpectedError(_) => Err(e500(e).into()),
+        };
     }
 
     todo!()
