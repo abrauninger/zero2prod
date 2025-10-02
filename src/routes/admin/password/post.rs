c907e95c08f98d3b53cab0dbb77ad44803efe56f
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-02 14:22:53 -0700

    10.9: Make 'reject_anonymous_users' middleware available on all 'admin' routes

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -5,7 +5,7 @@ use sqlx::PgPool;
 use uuid::Uuid;
 
 use crate::{
-    authentication::{AuthError, Credentials, validate_credentials},
+    authentication::{AuthError, Credentials, UserId, validate_credentials},
     routes::admin::dashboard::get_username,
     session_state::TypedSession,
     utils::{e500, see_other},
@@ -20,10 +20,10 @@ pub struct FormData {
 
 pub async fn change_password(
     form: web::Form<FormData>,
-    session: TypedSession,
     pool: web::Data<PgPool>,
+    user_id: web::ReqData<UserId>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let user_id = get_logged_in_user_id(&session).await?;
+    let user_id = user_id.into_inner();
 
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
@@ -33,7 +33,7 @@ pub async fn change_password(
         return Ok(see_other("/admin/password"));
     }
 
-    let username = get_username(user_id, &pool).await.map_err(e500)?;
+    let username = get_username(*user_id, &pool).await.map_err(e500)?;
 
     let credentials = Credentials {
         username,
@@ -50,7 +50,7 @@ pub async fn change_password(
         };
     }
 
-    crate::authentication::change_password(user_id, form.0.new_password, &pool)
+    crate::authentication::change_password(*user_id, form.0.new_password, &pool)
         .await
         .map_err(e500)?;
     FlashMessage::info("Your password has been changed.").send();
