694c2bf4214a1d4230728e96e148d064dafd134c
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-07 11:48:02 -0700

    11.10: Spawn separate tasks for 'application' and 'worker'

--- src/main.rs
+++ src/main.rs
@@ -28,13 +28,12 @@ async fn main() {
 
     let configuration = get_configuration();
 
-    let application = Application::build(configuration.clone())
-        .await
-        .run_until_stopped();
-    let worker = run_worker_until_stopped(configuration);
+    let application = Application::build(configuration.clone()).await;
+    let application_task = tokio::spawn(application.run_until_stopped());
+    let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
 
     tokio::select! {
-        _ = application => {},
-        _ = worker => {},
+        _ = application_task => {},
+        _ = worker_task => {},
     };
 }
