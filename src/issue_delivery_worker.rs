5f32e52e164e3f1e0f410ae043d4e3b60545f123
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-07 11:36:19 -0700

    11.10: Begin to implement 'worker_loop'

--- src/issue_delivery_worker.rs
+++ src/issue_delivery_worker.rs
@@ -1,47 +1,61 @@
+use std::time::Duration;
+
 use sqlx::{Executor, PgPool, Postgres, Transaction};
 use tracing::{Span, field::display};
 use uuid::Uuid;
 
 use crate::{domain::SubscriberEmail, email_client::EmailClient};
 
-#[tracing::instrument(skip_all, fields(newsletter_issue_id=tracing::field::Empty, subscriber_email=tracing::field::Empty), err)]
-async fn try_execute_task(pool: &PgPool, email_client: &EmailClient) -> Result<(), anyhow::Error> {
-    if let Some((transaction, issue_id, email)) = dequeue_task(pool).await? {
-        Span::current()
-            .record("newsletter_issue_id", display(issue_id))
-            .record("subscriber_email", display(&email));
+enum ExecutionOutcome {
+    TaskCompleted,
+    EmptyQueue,
+}
 
-        match SubscriberEmail::parse(email.clone()) {
-            Ok(email) => {
-                let issue = get_issue(pool, issue_id).await?;
-                if let Err(e) = email_client
-                    .send_email(
-                        &email,
-                        &issue.title,
-                        &issue.content_html,
-                        &issue.content_text,
-                    )
-                    .await
-                {
-                    tracing::error!(
-                        error.cause_chain = ?e,
-                        error.message = %e,
-                        "Failed to deliver issue to a confirmed subscriber. Skipping.",
-                    );
-                }
-            }
-            Err(e) => {
+#[tracing::instrument(skip_all, fields(newsletter_issue_id=tracing::field::Empty, subscriber_email=tracing::field::Empty), err)]
+async fn try_execute_task(
+    pool: &PgPool,
+    email_client: &EmailClient,
+) -> Result<ExecutionOutcome, anyhow::Error> {
+    let task = dequeue_task(pool).await?;
+    let Some((transaction, issue_id, email)) = task else {
+        return Ok(ExecutionOutcome::EmptyQueue);
+    };
+
+    Span::current()
+        .record("newsletter_issue_id", display(issue_id))
+        .record("subscriber_email", display(&email));
+
+    match SubscriberEmail::parse(email.clone()) {
+        Ok(email) => {
+            let issue = get_issue(pool, issue_id).await?;
+            if let Err(e) = email_client
+                .send_email(
+                    &email,
+                    &issue.title,
+                    &issue.content_html,
+                    &issue.content_text,
+                )
+                .await
+            {
                 tracing::error!(
                     error.cause_chain = ?e,
                     error.message = %e,
-                    "Skipping a confirmed subscriber. Their stored contact details are invalid",
+                    "Failed to deliver issue to a confirmed subscriber. Skipping.",
                 );
             }
         }
-
-        delete_task(transaction, issue_id, &email).await?;
+        Err(e) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "Skipping a confirmed subscriber. Their stored contact details are invalid",
+            );
+        }
     }
-    Ok(())
+
+    delete_task(transaction, issue_id, &email).await?;
+
+    Ok(ExecutionOutcome::TaskCompleted)
 }
 
 type PgTransaction = Transaction<'static, Postgres>;
@@ -116,4 +130,19 @@ async fn get_issue(pool: &PgPool, issue_id: Uuid) -> Result<NewsletterIssue, any
     .fetch_one(pool)
     .await?;
     Ok(issue)
+}
+
+async fn worker_loop(pool: PgPool, email_client: EmailClient) -> Result<(), anyhow::Error> {
+    loop {
+        // TODO: p. 578: Don't sleep if 'try_execute_task' fails with a non-transient error
+        match try_execute_task(&pool, &email_client).await {
+            Ok(ExecutionOutcome::EmptyQueue) => {
+                tokio::time::sleep(Duration::from_secs(10)).await;
+            }
+            Err(_) => {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            }
+            Ok(ExecutionOutcome::TaskCompleted) => {}
+        }
+    }
 }
