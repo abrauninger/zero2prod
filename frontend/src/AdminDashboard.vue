6b45d76ba75a570abc5b07363527b5e18ac5a3ba
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-08 13:51:47 -0700

    Redirect when 401 response is received

--- frontend/src/AdminDashboard.vue
+++ frontend/src/AdminDashboard.vue
@@ -5,17 +5,26 @@
 
 <script setup lang="ts">
 import { ref, watchEffect } from 'vue'
+import { useRouter } from 'vue-router'
 import { getUsername } from './api.ts'
 import type { Ref } from 'vue'
 
 const username: Ref<string | null> = ref(null)
+const router = useRouter()
 
 watchEffect(async () => {
   const response = await getUsername()
 
   if (!response.ok) {
-    username.value = "Couldn't get it!"
+    if (response.status == 401) {
+      // TODO: Redirect to '/login'
+      router.replace('/')
+    } else {
+      username.value = "Couldn't get it!"
+    }
   } else {
+    const responseContent = await response.json()
+    console.log(responseContent)
     username.value = 'Got it!'
   }
 })
