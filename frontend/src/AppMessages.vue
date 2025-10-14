d93d54db643e9b6c392c80764ab08c6fb92a7e47
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-14 10:18:56 -0700

    Use Tailwind styling for AppMessages

--- frontend/src/AppMessages.vue
+++ frontend/src/AppMessages.vue
@@ -1,9 +1,15 @@
 <template>
-  <div v-if="errorMessage" class="error-message">
+  <div
+    v-if="errorMessage"
+    class="text-red-900 bg-red-200 border border-solid border-red-900 p-2 mt-2"
+  >
     {{ errorMessage }}
   </div>
 
-  <div v-if="infoMessage" class="info-message">
+  <div
+    v-if="infoMessage"
+    class="text-green-900 bg-green-200 border border-solid border-green-900 p-2 mt-2"
+  >
     {{ infoMessage }}
   </div>
 </template>
