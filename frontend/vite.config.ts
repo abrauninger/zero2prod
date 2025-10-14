0a166defb17b0faacbf8f8f2da3d7adfb40a9783
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-13 23:21:51 -0700

    Enable vue-tsc errors in the dev overlay (in the browser window)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -8,7 +9,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    tailwindcss(),
+    checker({ vueTsc: { tsconfigPath: './tsconfig.app.json' } }),
+  ],
   server: {
     proxy: {
       '/api': {
