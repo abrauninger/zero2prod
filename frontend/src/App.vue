66ffc095a9bc40dfeea6905836aed6d7b57e9343
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-14 16:21:48 -0700

    Add padding to navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <nav class="relative bg-gray-200">
+  <nav class="relative bg-gray-200 p-2">
     <div class="flex space-x-4">
       <RouterLink
         to="/"
-        class="rounded-md px-3 py-2 text-sm font-medium bg-gray-300 text-black hover:bg-gray-400 hover:text-red-600"
+        class="rounded-md px-3 py-2 text-sm font-medium bg-gray-300 text-black hover:bg-gray-400 hover:text-blue-800"
         >Home</RouterLink
       >
       <RouterLink
         to="/admin"
-        class="rounded-md px-3 py-2 text-sm font-medium bg-gray-300 text-black hover:bg-gray-400 hover:text-red-600"
+        class="rounded-md px-3 py-2 text-sm font-medium bg-gray-300 text-black hover:bg-gray-400 hover:text-blue-800"
         >Admin dashboard</RouterLink
       >
     </div>
