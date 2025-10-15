1f57372a32dfd8088468e2102d72c6b7bef9dd86
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2025-10-15 11:26:56 -0700

    Reduce code duplication in logged-in menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,46 +19,21 @@
             <MenuItems
               class="absolute right-0 w-56 origin-top-right bg-white rounded-md px-1 py-1 shadow-lg ring-1 ring-black/5 focus:outline-none"
             >
-              <MenuItem v-slot="{ active }" class="block">
-                <a
-                  @click="router.push('/')"
-                  :class="[
-                    active ? 'bg-blue-500 text-white' : 'text-gray-900',
-                    'cursor-default px-2 py-2 text-md rounded-md',
-                  ]"
-                  >Subscribe to newsletter</a
-                >
-              </MenuItem>
-              <MenuItem v-slot="{ active }" class="block">
-                <a
-                  @click="router.push('/admin/newsletters')"
-                  :class="[
-                    active ? 'bg-blue-500 text-white' : 'text-gray-900',
-                    'cursor-default px-2 py-2 text-md rounded-md',
-                  ]"
-                  >Send a newsletter issue</a
-                >
-              </MenuItem>
-              <MenuItem v-slot="{ active }" class="block">
-                <a
-                  @click="router.push('/admin/password')"
-                  :class="[
-                    active ? 'bg-blue-500 text-white' : 'text-gray-900',
-                    'cursor-default px-2 py-2 text-md rounded-md',
-                  ]"
-                  >Change password</a
-                >
-              </MenuItem>
-              <MenuItem v-slot="{ active }" class="block"
-                ><a
-                  @click="logout(router)"
-                  :class="[
-                    active ? 'bg-blue-500 text-white' : 'text-gray-900',
-                    'cursor-default px-2 py-2 text-md rounded-md',
-                  ]"
-                  >Log out</a
-                ></MenuItem
+              <MenuItem
+                v-for="command in loggedInMenuCommands"
+                v-slot="{ active }"
+                :key="command.route"
+                class="block"
               >
+                <a
+                  :class="[
+                    active ? 'bg-blue-500 text-white' : 'text-gray-900',
+                    'cursor-default px-2 py-2 text-md rounded-md',
+                  ]"
+                  @click="command.click ? command.click(router) : router.push(command.route)"
+                  >{{ command.label }}</a
+                >
+              </MenuItem>
             </MenuItems>
           </div>
         </Menu>
@@ -78,9 +53,10 @@
 </template>
 
 <script setup lang="ts">
-import { watchEffect } from 'vue'
+import { computed, watchEffect } from 'vue'
 
 import { RouterView, useRoute, useRouter } from 'vue-router'
+import type { Router } from 'vue-router'
 
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 import { ChevronDownIcon } from '@heroicons/vue/20/solid'
@@ -138,4 +114,17 @@ const generatedBreadcrumbs = () => {
   console.log(breadcrumbs)
   return breadcrumbs
 }
+
+const loggedInMenuCommands = computed(() => {
+  if (username.value) {
+    return [
+      { label: 'Subscribe to newsletter', route: '/' },
+      { label: 'Send a newsletter issue', route: '/admin/newsletters' },
+      { label: 'Change password', route: '/admin/password' },
+      { label: 'Log out', click: (router: Router) => logout(router) },
+    ]
+  } else {
+    return []
+  }
+})
 </script>
